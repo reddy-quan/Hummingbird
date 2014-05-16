acdb83ed24383fd8411fa0b3bfbb164cfc511f3b
reddy <reddy_quan@sina.com>
2014-05-16 18:43:45 +0800

    1

---
+++ call_build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#export PATH=~/bird/MerriiLinux/boot/config/gcc-linaro/bin:$PATH:~/bird/MerriiLinux/u-boot/tools
+path=$(pwd)
+export PATH=$path/boot/config/gcc-linaro/bin:$PATH:$path/u-boot/tools
+echo "AAAAA: $PATH"
+
+#mv -f build.log build.log.old | tee -a build.log
+rm -rf build.log build.log.old | tee -a build.log
+sh ./build.sh -p sun7i_dragonboard | tee -a build.log
+
+#./build.sh pack
+
+#mkdir -p images
+#cp -rf tools/pack/sun7i_dragonboard_wing-k70.img images/sun7i_dragonboard_wing-k70.img
+
+echo "rootfs is at: ./buildroot/target/dragonboard/rootfs"
